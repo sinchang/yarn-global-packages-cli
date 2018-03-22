19dc07d917997cc7aaad97745980770c53b73a2a
sinchang <sinchangwen@gmail.com>
2018-03-22 13:24:34 +0800

    feat: add update notifier

--- cli.js
+++ cli.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 'use strict'
 const cac = require('cac')
+const updateNotifier = require('update-notifier')
 const main = require('./')
-
+const pkg = require('./package.json')
 const cli = cac()
+
+updateNotifier({pkg}).notify()
 
 cli.command('*', 'My Default Command', (input, flags) => {
   console.log(main())
+  process.exit()
 })
 
 cli.parse()
